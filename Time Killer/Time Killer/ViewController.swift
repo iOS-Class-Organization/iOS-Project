76e950f25dbb904055f27745a607f1fd9b0b2228
John W. Smith, V <jwsv61099@gmail.com>
2021-04-01 15:21:38 -0400

    Created the XCode Project

---
+++ Time Killer/Time Killer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time Killer
+//
+//  Created by John Smith V on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
